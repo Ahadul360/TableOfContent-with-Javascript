4870d2334fc21ac2abaf0478b3818479420fd0f7
Ahadul <ahadul.ais@gmail.com>
2020-06-21 13:08:15 +0600

    All header filter by TagName

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const allPostHeader = document.getElementById("post").children;
 let tableOfContent="";
 for(let i=0; i<allPostHeader.length; i++){
-    if(allPostHeader[i].className == "post-header"){
+    if(allPostHeader[i].tagName == "H1" || allPostHeader[i].tagName == "H2" || allPostHeader[i].tagName == "H3" || allPostHeader[i].tagName == "H4"){
         allPostHeader[i].id = "post-header"+i;
         tableOfContent += '<li><a href="#post-header'+i+'">'+allPostHeader[i].innerHTML+'</a></li>';
     }
 }
+console.log(allPostHeader)
 document.getElementById("tableOfContentContainer").innerHTML = tableOfContent;
